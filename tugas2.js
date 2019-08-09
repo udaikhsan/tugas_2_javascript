b0a5505631bac9caa23665d3fdcb442d55b1bf14
Ikhsan <ikhsannurdiansyah4@gmail.com>
2019-08-09 22:13:18 +0700

    Tugas 2 Javascript
    Tugas ini saya kumpulkan untuk NIOMIC

---
+++ tugas2.js
@@ -0,0 +1,18 @@
+var number = 30.5;
+
+var string = "Tugas 2 Javascript";
+
+var boolean = 111;
+if (boolean > 110) boolean = true;
+else boolean = false;
+
+var tugas = "javascript";
+tugas = null;
+
+var DataArray = [7, 6, 5, 4, 3, 2];
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(tugas);
+console.log(DataArray[2]);
